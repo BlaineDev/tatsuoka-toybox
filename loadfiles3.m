b95659d9acb63996fada2149da612eb9951a9d42
BlaineDev <43523756+BlaineDev@users.noreply.github.com>
2020-07-20 00:23:05 -0400

    Update loadfiles3.m

--- loadfiles3.m
+++ loadfiles3.m
@@ -39,24 +39,13 @@ fname2 = {  'pat1.txt', ...
         };    
              
 
-
-
-
-
-
-
-
 %%
-
     
 for i = 1:fileJ
       fname1{i} = readmatrix(fname1{i});   %opens the content of each filename, reusing the array locations 
       A0{i} = fname1{i}+ triu(fname1{i},1)'; %convert triangle matrix to square, into a new array %M = M + triu(M,1)';
-     % A0_eigens{i} = eig(A0{i});  % get eigenvalues @blaine: of Graph
-     % Laplacian
-     % A0_eigen_max{i} = max(A0_eigens{i});  %get max eigenvalue
+  
 end
-
 
 
 %%
@@ -64,15 +53,9 @@ end
  for i = 1:fileK
       fname2{i} = readmatrix(fname2{i});   %opens the content of each filename, reusing the array locations 
       A1{i} = fname2{i}+ triu(fname2{i},1)'; %convert triangle matrix to square, into a new array %M = M + triu(M,1)';
-      %A1_eigens{i} = eig(A1{i});  % get eigenvalues
-      %A1_eigen_max{i} = max(A1_eigens{i});  %get max eigenvalue
+     
  end
 
- 
-A4 = A0_eigen_max';  %note transpose 
-A5 = A1_eigen_max';
 
-A4 = max([A4{:}]);  %find max value
-A5 = max([A5{:}]);
-disp(A4); 
-disp(A5);
+disp(A0); 
+disp(A1);
